ea6d47a313db556338b70533b6294a2ae3ff582a
त्रिकालदर्शी <86564011+trikaaldarshi@users.noreply.github.com>
2025-08-29 14:17:52 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -100,6 +100,21 @@ async def start(client, message):
         buttons = [[
                     InlineKeyboardButton('🔰 ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ 🔰', url=f'http://telegram.me/{temp.U_NAME}?startgroup=true')
                 ],[
+                                InlineKeyboardButton('「 ✦ ᴏᴡɴᴇʀ ✦ 」', url='http://t.me/chat2ownerbot')
+                ],[
+                    InlineKeyboardButton('✎ᝰ.ʜᴇʟᴘ', callback_data='help'), InlineKeyboardButton('ⓘ ᴀʙᴏᴜᴛ', callback_data='about')
+                ],[ 
+                    InlineKeyboardButton('〄 ᴄʜᴀɴɴᴇʟ 〄', url='https://t.me/CINBox'), 
+                    InlineKeyboardButton('〄 ɢʀᴏᴜᴘ 〄', url='https://t.me/iPopkornMovies_Group')
+                ],[
+                    InlineKeyboardButton('Backup', url='https://t.me/iPopcornbotNews')
+                ],[
+                    InlineKeyboardButton(' ʜᴇʟᴘ 📢', callback_data='help'),
+                    InlineKeyboardButton(' ᴀʙᴏᴜᴛ 📖', callback_data='about')
+                ],[
+                    InlineKeyboardButton('ᴛᴏᴘ sᴇᴀʀᴄʜɪɴɢ ⭐', callback_data="topsearch"),
+                    InlineKeyboardButton('ᴜᴘɢʀᴀᴅᴇ 🎟', callback_data="premium_info"),
+                ]]
                     InlineKeyboardButton(' ʜᴇʟᴘ 📢', callback_data='help'),
                     InlineKeyboardButton(' ᴀʙᴏᴜᴛ 📖', callback_data='about')
                 ],[
