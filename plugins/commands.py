f02b05fc2ed4fa22f3112a571627c37bdf973bee
त्रिकालदर्शी <86564011+trikaaldarshi@users.noreply.github.com>
2025-08-30 08:17:25 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -99,21 +99,18 @@ async def start(client, message):
     if len(message.command) != 2:
         buttons = [[
                     InlineKeyboardButton('🔰 ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ 🔰', url=f'http://telegram.me/{temp.U_NAME}?startgroup=true')
+                ],[                
+                   InlineKeyboardButton('「 ✦ ᴏᴡɴᴇʀ ✦ 」', url='http://t.me/chat2ownerbot')
                 ],[
-                                InlineKeyboardButton('「 ✦ ᴏᴡɴᴇʀ ✦ 」', url='http://t.me/chat2ownerbot')
-                ],[
-                    InlineKeyboardButton('✎ᝰ.ʜᴇʟᴘ', callback_data='help'), InlineKeyboardButton('ⓘ ᴀʙᴏᴜᴛ', callback_data='about')
+                    InlineKeyboardButton('✎ᝰ.ʜᴇʟᴘ/सहायता', callback_data='help'), InlineKeyboardButton('ⓘ ᴀʙᴏᴜᴛ', callback_data='about')
                 ],[ 
                     InlineKeyboardButton('〄 ᴄʜᴀɴɴᴇʟ 〄', url='https://t.me/CINBox'), 
-                    InlineKeyboardButton('〄 ɢʀᴏᴜᴘ 〄', url='https://t.me/iPopkornMovies_Group')
+                    InlineKeyboardButton('〄 ɢʀᴏᴜᴘ 〄 (ग्रुप) ', url='https://t.me/iPopkornMovies_Group')
                 ],[
-                    InlineKeyboardButton('Backup', url='https://t.me/iPopcornbotNews')
-                ],[
-                    InlineKeyboardButton(' ʜᴇʟᴘ 📢', callback_data='help'),
-                    InlineKeyboardButton(' ᴀʙᴏᴜᴛ 📖', callback_data='about')
-                ],[
-                    InlineKeyboardButton('ᴛᴏᴘ sᴇᴀʀᴄʜɪɴɢ ⭐', callback_data="topsearch"),
-                    InlineKeyboardButton('ᴜᴘɢʀᴀᴅᴇ 🎟', callback_data="premium_info"),
+                    InlineKeyboardButton('Backup(बैकअप)', url='https://t.me/iPopcornbotNews')
+
+                ],[              
+                     InlineKeyboardButton('ᴜᴘɢʀᴀᴅᴇ 🎟', callback_data="premium_info"),
                 ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         current_time = datetime.now(pytz.timezone(TIMEZONE))
@@ -140,21 +137,17 @@ async def start(client, message):
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
                     InlineKeyboardButton('🔰 ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ 🔰', url=f'http://telegram.me/{temp.U_NAME}?startgroup=true')
+                ],[                
+                   InlineKeyboardButton('「 ✦ ᴏᴡɴᴇʀ ✦ 」', url='http://t.me/chat2ownerbot')
                 ],[
-                    InlineKeyboardButton('「 ✦ ᴏᴡɴᴇʀ ✦ 」', url='http://t.me/chat2ownerbot')
-                ],[
-                    InlineKeyboardButton('✎ᝰ.ʜᴇʟᴘ', callback_data='help'), InlineKeyboardButton('ⓘ ᴀʙᴏᴜᴛ', callback_data='about')
+                    InlineKeyboardButton('✎ᝰ.ʜᴇʟᴘ/सहायता', callback_data='help'), InlineKeyboardButton('ⓘ ᴀʙᴏᴜᴛ', callback_data='about')
                 ],[ 
                     InlineKeyboardButton('〄 ᴄʜᴀɴɴᴇʟ 〄', url='https://t.me/CINBox'), 
-                    InlineKeyboardButton('〄 ɢʀᴏᴜᴘ 〄', url='https://t.me/iPopkornMovies_Group')
+                    InlineKeyboardButton('〄 ɢʀᴏᴜᴘ 〄 (ग्रुप) ', url='https://t.me/iPopkornMovies_Group')
                 ],[
-                    InlineKeyboardButton('Backup', url='https://t.me/iPopcornbotNews')
-                ],[
-                    InlineKeyboardButton(' ʜᴇʟᴘ 📢', callback_data='help'),
-                    InlineKeyboardButton(' ᴀʙᴏᴜᴛ 📖', callback_data='about')
-                ],[
-                    InlineKeyboardButton('ᴛᴏᴘ sᴇᴀʀᴄʜɪɴɢ ⭐', callback_data="topsearch"),
-                    InlineKeyboardButton('ᴜᴘɢʀᴀᴅᴇ 🎟', callback_data="premium_info"),
+                    InlineKeyboardButton('Backup(बैकअप)', url='https://t.me/iPopcornbotNews')
+                ],[                
+                     InlineKeyboardButton('ᴜᴘɢʀᴀᴅᴇ 🎟', callback_data="premium_info"),
                 ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         current_time = datetime.now(pytz.timezone(TIMEZONE))
